53886665af2a87e1b9c214f0624d8f3224afc84b
Siraj Qazi <siraj.qazi@seeloz.com>
2023-07-12 21:00:02 +0500

    added sample driver

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "DataFrame.h"
+
+
+int main() {
+    qz::DataFrame df;
+
+    // read data from a CSV file
+    df.read_csv("../data.csv");
+
+    // print DataFrame
+    std::cout << df << std::endl;
+    return 0;
+}
